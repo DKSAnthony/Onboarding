3d04763bd2da7989b202711588adecb0bd21fa5a
DKSAnthony <98044583+DKSAnthony@users.noreply.github.com>
2022-01-21 17:36:05 -0500

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Olukitibi, Anthony on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
