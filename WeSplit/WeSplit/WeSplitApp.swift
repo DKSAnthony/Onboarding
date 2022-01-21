24ea8db74af3574de2491b429b23141b729b6271
DKSAnthony <98044583+DKSAnthony@users.noreply.github.com>
2022-01-21 17:33:28 -0500

    Add files via upload

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Olukitibi, Anthony on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
