43a645b493e54de0920940994e14846184100551
DKSAnthony <98044583+DKSAnthony@users.noreply.github.com>
2022-01-22 22:09:01 -0500

    Add files via upload

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Olukitibi, Anthony on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
