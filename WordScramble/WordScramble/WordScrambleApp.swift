5afe72fa80e42ba449f24355f85f1f4e5ef152c8
DKSAnthony <98044583+DKSAnthony@users.noreply.github.com>
2022-01-22 22:09:44 -0500

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Olukitibi, Anthony on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
