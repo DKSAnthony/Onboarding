d89ca808da5eba4c9cbd2b6bf5434fae50936de0
DKSAnthony <98044583+DKSAnthony@users.noreply.github.com>
2022-01-24 16:46:27 -0500

    Add files via upload

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Olukitibi, Anthony on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
