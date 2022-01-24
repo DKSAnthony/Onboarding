4234152f982d436247cf3b3fc5ea83d1e994cd16
DKSAnthony <98044583+DKSAnthony@users.noreply.github.com>
2022-01-23 22:22:11 -0500

    Add files via upload

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Olukitibi, Anthony on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
