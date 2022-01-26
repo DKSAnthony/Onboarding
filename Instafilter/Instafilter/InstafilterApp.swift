3aa752c19e4696a2648f2caa6b9e170e836f1430
DKSAnthony <98044583+DKSAnthony@users.noreply.github.com>
2022-01-25 23:22:36 -0500

    Add files via upload

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Olukitibi, Anthony on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
