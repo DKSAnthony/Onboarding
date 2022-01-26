f88ca18f76b251c3178f04de4c0e2c244731d566
DKSAnthony <98044583+DKSAnthony@users.noreply.github.com>
2022-01-25 23:23:15 -0500

    Add files via upload

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Olukitibi, Anthony on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
